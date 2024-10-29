c5bcbc1272d580f6379de8ae216614c1e4abf5ca
Nihas <nihasnizar@gmail.com>
2024-10-29 17:34:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import MemoryGame from './MemoryGame'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>Memory Game</h1>
+      <MemoryGame/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
